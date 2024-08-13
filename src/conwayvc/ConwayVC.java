f2c51cdff6544b037189648279a0c2380e7b94d7
Prof Anderson Santos, PhD <anderson0563@gmail.com>
2024-08-13 05:51:59 -0300

    Add files via upload

---
+++ src/conwayvc/ConwayVC.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conwayvc;
+
+/**
+ *
+ * @author LH
+ */
+public class ConwayVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=1;
+        System.out.println("123123123");
+    }
+    
+}
